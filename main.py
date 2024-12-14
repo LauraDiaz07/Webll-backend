87188c8c9f98394d5990012fa51a6c6a091341f1
Alejandro Larreta Vzla <larreta_vzla@hotmail.com>
2024-12-13 23:18:31 -0700

    Mas cosas no se 2

--- main.py
+++ main.py
@@ -17,18 +17,18 @@ if pruebas:
     app.config['MYSQL_HOST'] = 'localhost'
     app.config['MYSQL_PORT'] = 3306
 else:
-    app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-    app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-    app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-    app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
-    app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', 3306))
-    app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-    # app.config['MYSQL_HOST'] = 'junction.proxy.rlwy.net'
-    # app.config['MYSQL_USER'] = 'root'
-    # app.config['MYSQL_PASSWORD'] = 'TtdAwfxuOnwpKNbFlinUQuLHrvsMswCu'
-    # app.config['MYSQL_DB'] = 'railway'
-    # app.config['MYSQL_PORT'] = 33070
+    # app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+    # app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+    # app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+    # app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+    # app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', 3306))
     # app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+    app.config['MYSQL_HOST'] = 'junction.proxy.rlwy.net'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = 'TtdAwfxuOnwpKNbFlinUQuLHrvsMswCu'
+    app.config['MYSQL_DB'] = 'railway'
+    app.config['MYSQL_PORT'] = 33070
+    app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
 
